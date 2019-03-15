317061d7624f0b7a53a0b1f8e388e7448b35d2ff
MANSI <mansi.sharma@mail.vinove.com>
2019-03-16 00:02:11 +0530

    Change in goPayment method

--- app/src/main/java/com/example/mansi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mansi/myapplication/MainActivity.java
@@ -20,42 +20,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void goPayment() {
-
         Intent in = new Intent(getApplicationContext(), PayTabActivity.class);
-        in.putExtra(PaymentParams.MERCHANT_EMAIL, "alrefaiy.mohammed@gmail.com"); //this a demo account for testing the sdk
-        in.putExtra(PaymentParams.SECRET_KEY,"Xw5ny7zjNhGFkTv11TEDvc8SBB3TMEDKaJ7JQH2DZIijCOlEIZhdcp5N18GNMkxX3FGUnJxmsQG4F1In10AdqG0rPHeB2Mz5rf0O");//Add your Secret Key Here
-        in.putExtra(PaymentParams.LANGUAGE,PaymentParams.ENGLISH);
-        in.putExtra(PaymentParams.TRANSACTION_TITLE, "Test Paytabs android library");
-        in.putExtra(PaymentParams.AMOUNT, 5.0);
+// Merchant
+        in.putExtra(PaymentParams.MERCHANT_EMAIL, "alrefaiy.mohammed@gmail.com");
+        in.putExtra(PaymentParams.SECRET_KEY, "Xw5ny7zjNhGFkTv11TEDvc8SBB3TMEDKaJ7JQH2DZIijCOlEIZhdcp5N18GNMkxX3FGUnJxmsQG4F1In10AdqG0rPHeB2Mz5rf0O"); //Add your Secret Key Here
+        in.putExtra(PaymentParams.LANGUAGE, PaymentParams.ENGLISH);
 
+        in.putExtra(PaymentParams.PAY_BUTTON_COLOR, "#0075c9");
+        in.putExtra(PaymentParams.THEME, PaymentParams.THEME_LIGHT);
+// Transaction
+        in.putExtra(PaymentParams.TRANSACTION_TITLE, "My application title");
+        in.putExtra(PaymentParams.PRODUCT_NAME, "Product name");
+        in.putExtra(PaymentParams.AMOUNT, 3.5);
         in.putExtra(PaymentParams.CURRENCY_CODE, "BHD");
-        in.putExtra(PaymentParams.CUSTOMER_PHONE_NUMBER, "009733");
-        in.putExtra(PaymentParams.CUSTOMER_EMAIL, "customer-email@example.com");
         in.putExtra(PaymentParams.ORDER_ID, "123456");
-        in.putExtra(PaymentParams.PRODUCT_NAME, "Product 1, Product 2");
-
-//Billing Address
-        in.putExtra(PaymentParams.ADDRESS_BILLING, "Flat 1,Building 123, Road 2345");
+// Customer
+        in.putExtra(PaymentParams.CUSTOMER_PHONE_NUMBER, "009733");
+        in.putExtra(PaymentParams.CUSTOMER_EMAIL, "j.deo@example.com");
+// Billing Address
+        in.putExtra(PaymentParams.ADDRESS_BILLING, "Flat 11 Building 222 Block 333 Road 444 Manama Bahrain");
         in.putExtra(PaymentParams.CITY_BILLING, "Manama");
         in.putExtra(PaymentParams.STATE_BILLING, "Manama");
         in.putExtra(PaymentParams.COUNTRY_BILLING, "BHR");
-        in.putExtra(PaymentParams.POSTAL_CODE_BILLING, "00973"); //Put Country Phone code if Postal code not available '00973'
-
-//Shipping Address
-        in.putExtra(PaymentParams.ADDRESS_SHIPPING, "Flat 1,Building 123, Road 2345");
+        in.putExtra(PaymentParams.POSTAL_CODE_BILLING, "12345"); //Put Country Phone code if Postal code not available '00973'
+// Shipping Address
+        in.putExtra(PaymentParams.ADDRESS_SHIPPING, "Flat 11 Building 222 Block 333 Road 444 Manama Bahrain");
         in.putExtra(PaymentParams.CITY_SHIPPING, "Manama");
         in.putExtra(PaymentParams.STATE_SHIPPING, "Manama");
         in.putExtra(PaymentParams.COUNTRY_SHIPPING, "BHR");
-        in.putExtra(PaymentParams.POSTAL_CODE_SHIPPING, "00973"); //Put Country Phone code if Postal code not available '00973'
-
-//Payment Page Style
-        in.putExtra(PaymentParams.PAY_BUTTON_COLOR, "#2474bc");
-        in.putExtra(PaymentParams.THEME, PaymentParams.THEME_LIGHT);
-
+        in.putExtra(PaymentParams.POSTAL_CODE_SHIPPING, "12345"); //Put Country Phone code if Postal code not available '00973'
 //Tokenization
         in.putExtra(PaymentParams.IS_TOKENIZATION, true);
         startActivityForResult(in, PaymentParams.PAYMENT_REQUEST_CODE);
+
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
